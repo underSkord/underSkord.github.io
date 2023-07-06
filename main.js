28c3a08214942104c02050c1a99588d4a240a61a
Peter <85614837+underSkord@users.noreply.github.com>
2023-07-06 13:41:34 +0200

    add function for better readabaility

--- main.js
+++ main.js
@@ -1,40 +1,37 @@
-// SelecTHORS   hehehe
+// buttons
 const item1Btn = document.querySelector('#item__1-btn');
 const item2Btn = document.querySelector('#item__2-btn');
 const item3Btn = document.querySelector('#item__3-btn');
 const item4Btn = document.querySelector('#item__4-btn');
 
+// info panels
 const item1Info = document.querySelector('#item-1__info');
 const item2Info = document.querySelector('#item-2__info');
 const item3Info = document.querySelector('#item-3__info');
 const item4Info = document.querySelector('#item-4__info');
 
+// functions
+const toggleInfo = (button, info, projectNum) => {
+    if (info.style.display === "none") {
+      info.style.display = "block";
+      button.textContent = `Close ${projectNum}`;
+    } 
+    else {
+      info.style.display = "none";
+      button.textContent = "Expand";
+  };
+};
+
+// buttons
 item1Btn.addEventListener('click', () => {
-    if (item1Info.style.display === "none") {
-        item1Info.style.display = "block";
-    } else {
-        item1Info.style.display = "none";
-    }
+    toggleInfo(item1Btn, item1Info, 'Project 1');
 });
 item2Btn.addEventListener('click', () => {
-    if (item2Info.style.display === "none") {
-        item2Info.style.display = "block";
-    } else {
-        item2Info.style.display = "none";
-    }
+    toggleInfo(item2Btn, item2Info, 'Project 2');
 });
 item3Btn.addEventListener('click', () => {
-    if (item3Info.style.display === "none") {
-        item3Info.style.display = "block";
-    } else {
-        item3Info.style.display = "none";
-    }
+    toggleInfo(item3Btn, item3Info, 'Project 3');
 });
 item4Btn.addEventListener('click', () => {
-    if (item4Info.style.display === "none") {
-        item4Info.style.display = "block";
-    } else {
-        item4Info.style.display = "none";
-    }
+    toggleInfo(item4Btn, item4Info, 'Project 4');
 });
-
